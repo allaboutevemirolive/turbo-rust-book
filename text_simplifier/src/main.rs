2c0f708ac6bd6acd99c6a23a6df8de6d42328a91
allaboutevemirolive <xkmxlfirdxus@gmail.com>
2023-06-10 05:19:42 -0400

    update

--- text_simplifier/src/main.rs
+++ text_simplifier/src/main.rs
@@ -4,7 +4,7 @@ use std::path::{Path, PathBuf};
 
 fn main() {
     let input_file     = "input.txt";
-    let output_file    = "10.3. Validating References with Lifetimes.md";
+    let output_file    = "11.1. How to Write Tests.md";
     let output_file = format_output_file_name(output_file);
 
     let data = read_input_file(input_file).expect("Unable to read input file");
